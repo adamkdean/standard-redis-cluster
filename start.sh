af24acd58c6b696c4b0690fa5694bf19ebbc7d33
Adam K Dean <adamkdean@googlemail.com>
2016-08-15 05:40:31 +0100

    initial commit of working cluster

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd 7000
+../redis-server ./redis.conf &
+
+cd ../7001
+../redis-server ./redis.conf &
+
+cd ../7002
+../redis-server ./redis.conf &
+
+cd ../7003
+../redis-server ./redis.conf &
+
+cd ../7004
+../redis-server ./redis.conf &
+
+cd ../7005
+../redis-server ./redis.conf &
